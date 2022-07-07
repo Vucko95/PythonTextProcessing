3dc48f233b9031d3fb4c14602ab5af1f42d6fe06
Ronald Suplina <tyger995@gmail.com>
2022-07-07 15:45:10 +0100

    Merge with single header

--- basic.py
+++ basic.py
@@ -1,22 +1,20 @@
 from pathlib import Path
 
-# with open('file3.csv', 'r') as file:
-#     content = file.read()
-
-# print(content[:-1])
-# modified_content = content[:-1]
-
-# with open('newfile3.csv', 'w') as file:
-#     file.write(modified_content)
-
-# Will Remove 1 word in file each time
 files_dir = Path('files')
 files = files_dir.iterdir()
 merged = ''
-for filepath in files:
-    # print(filepath)
+for index, filepath in enumerate(files):
     with open(filepath, 'r') as file:
-        content = file.read()
-    merged = merged + content
-with open('new.csv', 'w') as file:
-    file.write(merged)
+        content = file.readlines()
+        new_conent = content[1:]
+    if index == 0:
+        content = ''.join(content)
+        merged = merged + content + '\n'
+    else:
+        new_conent = ''.join(new_conent)
+        merged = merged + new_conent + '\n'
+    print(merged)
+    print('--')
+    # merged = merged + content
+    with open('new.csv', 'w') as file:
+        file.write(merged)
